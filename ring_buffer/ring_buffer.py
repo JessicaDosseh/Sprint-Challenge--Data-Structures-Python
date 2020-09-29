283d377cf92c977adccfd2e970e80151976df24f
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-09-29 10:34:18 -0400

    Task 1. Implement a Ring Buffer Data Structure

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        # List is empty
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        # List is not empty
+        else:
+            self.storage[self.index] = item
+        self.index += 1
+        # If capacity = index
+        if self.index == self.capacity:
+            self.index = 0
 
     def get(self):
-        pass
+        return self.storage
